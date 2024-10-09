8fa1bcfcbb52dc27a10d85b67bbd2d4bee760810
Yaroslav Zavyalov <yaroslavzavyalov1@gmail.com>
2024-10-09 21:00:42 +0300

    Создал проект

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -1,46 +1,16 @@
 package com.example.sharedpreferences
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sharedpreferences.ui.theme.SharedPreferencesTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sharedpreferences.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SharedPreferencesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SharedPreferencesTheme {
-        Greeting("Android")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
